2713418c9a84c3e47654e64b16900f56f25be5a7
Martins <adriel.m.martins@accenture.com>
2020-10-05 13:23:02 -0300

    Filtering propaganda from the Scrapping

--- LinkedIn.py
+++ LinkedIn.py
@@ -7,7 +7,10 @@ import pandas as pd
 
 class LinkedInBot:
     def __init__(self, webdriver_path):
-        self.driver = webdriver.Chrome(webdriver_path)
+        chrome_options = webdriver.chrome.options.Options()
+        chrome_options.add_argument('headless')
+
+        self.driver = webdriver.Chrome(webdriver_path, options=chrome_options)
         self.driver.get('https://www.linkedin.com/')
         sleep(5)
         
@@ -27,22 +30,36 @@ class LinkedInBot:
     def scroll_down(self):
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    def posts(self):
+    def _posts(self):
         posts = self.driver.find_elements_by_css_selector('.feed-shared-update-v2__commentary span')
         posts = list(set([post.text for post in posts]))
         return(posts)
 
-    def authors(self):
-        authors = self.driver.find_elements_by_css_selector('.feed-shared-actor__title .feed-shared-actor__name')
-        authors = list(set([author.text for author in authors]))
+    def _authors(self):
+        authors_name = self.driver.find_elements_by_css_selector('.feed-shared-actor__title .feed-shared-actor__name')
+        authors_titles = self.driver.find_elements_by_css_selector('.feed-shared-actor__description')
+        
+        authors_name = pd.Series([author.text for author in authors_name]).drop_duplicates().tolist()
+        authors_titles = pd.Series([author_title.text for author_title in authors_titles]).drop_duplicates().tolist()
+
+        authors = {
+            'name': authors_name,
+            'title': authors_titles
+        }
+
         return(authors)
 
     def df_author_post(self):
-        res = pd.DataFrame(
-            {'authors' : self.authors(),
-            'post': self.posts()}
+        df = pd.DataFrame(
+            {'author_name': self._authors().get('name'),
+            'author_title': self._authors().get('title'),
+            'post': self._posts()}
         )
-        return(res)
+
+        df.propaganda = df.apply(lambda row: 'followers' in row.author_title, axis=1)
+        df = df[df.propaganda == False]
+
+        return(df)
     
     def quit(self):
         sleep(1)
@@ -56,6 +73,11 @@ bot = LinkedInBot(webdriver_path_windows)
 bot.sign_in(secret.username, secret.password)
 sleep(1)
 
-print(bot.df_author_post())
+for i in range(3):
+    bot.scroll_down()
+
+df = bot.df_author_post()
+print(df)
 
 bot.quit()
+
