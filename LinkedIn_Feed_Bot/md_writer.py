73018668d217941c5e9a9035583daa73df862f8b
Martins <adriel.m.martins@accenture.com>
2020-10-20 18:06:42 -0300

    Fixing some bugs and also automatically finding the filename if the user doesn't provide any output_file_path for the md_to_pdf

--- LinkedIn_Feed_Bot/md_writer.py
+++ LinkedIn_Feed_Bot/md_writer.py
@@ -1,34 +1,30 @@
 import pandas as pd
 import io
 
-def daily_feed_template_md(output_filename, pd_df = None, csv_path = None):
-    if(pd_df == None & csv_path == None):
-        raise Exception
-    df = pd.read_csv('data.csv')
+def feed_template_md(output_filename, input_obj):
+    """Writing out a simple template for the feed of LinkedIn.
+
+    Args:
+        output_filename (str)
+        input (pandas.DataFrame or str): If the input is a str,
+         it is assumed its a filepath to a csv file which will be read with pandas.
+         If, on the other hand, the input is apandas.DataFrame,
+         we will use it directly.
+
+    Returns:
+        [Boolean]: Boolean to make sure that the file is closed on our way out.
+    """
+    if(type(input_obj) != str and type(input_obj) != pd.core.frame.DataFrame):
+        raise ValueError('Input must be a str or a pandas.DataFrame')
+
+    if(type(input_obj) == str):
+        df = pd.read_csv(input_obj)
+    if(type(input_obj) == pd.core.frame.DataFrame):
+        df = input_obj
 
     f = io.open(output_filename, "w", encoding="utf-8")
 
-    f.write('# Daily Feed' + '\n')
-
-    for author, title, post in zip(df.author_name, df.author_title, df.post):
-        f.write('### ' + author + '\n')
-        f.write('### ' + title + '\n')
-        f.write('\n')
-        # To adjust the hashtags into the md file.
-        post = post.replace('#', '\#')
-        f.write(post + '\n')
-        f.write('\n')
-
-    f.close()
-    return f.closed
-
-
-def weekly_feed_template_md(output_filename, df):
-    df = pd.read_csv('data.csv')
-
-    f = io.open(output_filename, "w", encoding="utf-8")
-
-    f.write('# Weekly Feed' + '\n')
+    f.write('# LinkedIn Feed' + '\n')
 
     for author, title, post in zip(df.author_name, df.author_title, df.post):
         f.write('### ' + author + '\n')
